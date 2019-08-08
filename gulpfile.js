ef4de12fee9c7df641761cde82793cc2902ac6a3
SergeyBerez <sergeyberezovsky1981@gmail.com>
2019-08-08 14:37:44 +0300

    1

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require("gulp");
+var browserSync = require("browser-sync").create();
+
+// Static server
+gulp.task("server", function() {
+  browserSync.init({
+    server: {
+      port: 9000,
+      baseDir: "./"
+    }
+  });
+
+  gulp.watch("./**").on("change", browserSync.reload);
+});
