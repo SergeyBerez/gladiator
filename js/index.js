7e6614b209cbaf1649420c3ec7907fbc20cc9b67
SergeyBerez <sergeyberezovsky1981@gmail.com>
2019-08-28 16:29:49 +0300

    добавил баннер слайд карусель на главную страницу

--- js/index.js
+++ js/index.js
@@ -10,39 +10,40 @@ for (let i = 0; i < contentItems.length; i++) {
   }, 500);
 }
 
-(function() {
-  //модуль слайдера
+// (function() {
+//   //модуль слайдера
 
-  function Slider(elements, auto, time) {
-    this.element = elements;
-    var i = 0;
-    // this.btn = btn;
-    this.time = time || 1000;
-    this.auto = auto;
-    var slider = this;
-    slider.next = function() {
-      slider.element[i].classList.remove("show1");
-      i++;
+//   function Slider(elements, auto, time) {
+//     this.element = elements;
+//     var i = 0;
+//     // this.btn = btn;
+//     this.time = time || 1000;
+//     this.auto = auto;
+//     var slider = this;
+//     slider.next = function() {
+//       slider.element[i].classList.remove("show1");
+//       i++;
 
-      if (i >= slider.element.length) {
-        i = 0;
-      }
-      slider.element[i].classList.add("show1");
-    };
+//       if (i >= slider.element.length) {
+//         i = 0;
+//       }
+//       slider.element[i].classList.add("show1");
+//     };
 
-    if (this.auto == "auto") {
-      setInterval(slider.next, this.time);
-    }
+//     if (this.auto == "auto") {
+//       setInterval(slider.next, this.time);
+//     }
 
-    console.log(slider);
-  }
-  let contentItemsAll = document.querySelectorAll(".content-items");
-  console.log(contentItemsAll);
-  var newSlider = new Slider(contentItemsAll, "auto", 2000);
-})();
-// let contentItemsAll = document.querySelectorAll(".content-items");
-// console.log(contentItemsAll);
-// let i = 0;
+//     console.log(slider);
+//   }
+//   let contentItemsAll = document.querySelectorAll(".content-items");
+//   console.log(contentItemsAll);
+//   var newSlider = new Slider(contentItemsAll, "auto", 2000);
+// })();
+
+let contentItemsAll = document.querySelectorAll(".content-items");
+
+let slider = [];
 // setInterval(function() {
 //   contentItemsAll[i].classList.remove("show1");
 //   i++;
@@ -52,3 +53,49 @@ for (let i = 0; i < contentItems.length; i++) {
 //   }
 //   contentItemsAll[i].classList.add("show1");
 // }, 3000);
+for (let i = 0; i < contentItemsAll.length; i++) {
+  slider[i] = contentItemsAll[i];
+  contentItemsAll[i].remove();
+}
+console.log(slider[0]);
+console.log(contentItemsAll);
+let step = 0;
+let offset = 0;
+
+function draw(arguments) {
+  let div = document.createElement("div");
+  div = slider[step];
+  // div.classList.add("content-items");
+  div.style.left = offset * 400 + "px";
+  document.querySelector(".content-banner-wrap").appendChild(div);
+
+  if (step + 1 == slider.length) {
+    step = 0;
+  } else {
+    step++;
+  }
+  offset = 1;
+}
+
+function left(arguments) {
+  let contentItemsAll2 = document.querySelectorAll(".content-items");
+  let offset2 = 0;
+  for (let i = 0; i < contentItemsAll2.length; i++) {
+    contentItemsAll2[i].style.left = offset2 * 400 - 400 + "px";
+    offset2++;
+  }
+
+  setTimeout(function() {
+    contentItemsAll2[0].remove();
+    draw();
+  }, 1000);
+}
+draw();
+draw();
+
+// document.onclick = left;
+
+setInterval(function() {
+  left();
+  console.log(1);
+}, 2000);
